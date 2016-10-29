28622d7882c04548b081145fddb792e1539d3186
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-28 21:02:34 -0400

    Types in TS - string, number, boolean

--- ts-course/app.ts
+++ ts-course/app.ts
@@ -4,6 +4,15 @@
  */
 console.log("It works!");
 
+//string
 let myName = "Manish";
 //myName = 28;
 console.log(typeof myName);
+
+//number
+let myNumber = 20;
+//myNumber = 'a';
+
+//boolean
+let hasHobbies = false;
+//hasHobbies = 3;
