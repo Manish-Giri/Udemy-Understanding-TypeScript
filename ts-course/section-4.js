a2d5d028a7326ad49f19ed575be483d32e673c0b
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-30 19:48:16 -0400

    Create namespace for current ts file, Object destructuring with aliases

--- ts-course/section-4.js
+++ ts-course/section-4.js
@@ -1,47 +1,54 @@
 /**
  * Created by manishgiri on 10/30/16.
  */
-//ARROW FUNCTIONS
-console.log("ARROW FUNCTIONS");
-var addNumbers = function (num1, num2) {
-    return num1 + num2;
-};
-console.log(addNumbers(10, 15));
-var multipliedNumbers = function (num1, num2) { return num1 * num2; };
-console.log(multipliedNumbers(6, 7));
-var greeter = function () { return console.log("Hello World!!"); };
-greeter();
-var greetFriend = function (friend) { return console.log("Hi " + friend); };
-greetFriend("James");
-//default parameters
-var countDown = function (start) {
-    if (start === void 0) { start = 10; }
-    while (start > 0) {
-        console.log(start);
-        start--;
+var section4;
+(function (section4) {
+    //ARROW FUNCTIONS
+    console.log("ARROW FUNCTIONS");
+    var addNumbers = function (num1, num2) {
+        return num1 + num2;
+    };
+    console.log(addNumbers(10, 15));
+    var multipliedNumbers = function (num1, num2) { return num1 * num2; };
+    console.log(multipliedNumbers(6, 7));
+    var greeter = function () { return console.log("Hello World!!"); };
+    greeter();
+    var greetFriend = function (friend) { return console.log("Hi " + friend); };
+    greetFriend("James");
+    //default parameters
+    var countDown = function (start) {
+        if (start === void 0) { start = 10; }
+        while (start > 0) {
+            console.log(start);
+            start--;
+        }
+        console.log("Done! " + start);
+    };
+    //countDown(10);
+    //countDown();
+    //SPREAD OPERATOR
+    var numbers = [1, 20, 3, 55, 99];
+    console.log(Math.max(20, 3, 99, 55, 1));
+    console.log(Math.max.apply(Math, numbers));
+    //REST OPERATOR
+    function makeArray() {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i - 0] = arguments[_i];
+        }
+        return args;
     }
-    console.log("Done! " + start);
-};
-//countDown(10);
-//countDown();
-//SPREAD OPERATOR
-var numbers = [1, 20, 3, 55, 99];
-console.log(Math.max(20, 3, 99, 55, 1));
-console.log(Math.max.apply(Math, numbers));
-//REST OPERATOR
-function makeArray() {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i - 0] = arguments[_i];
-    }
-    return args;
-}
-console.log(makeArray(1, 2, 3, 4, 6));
-//IMP DIFFERENCE ->
-//1. When used in a function call, ... is used as a spread operator, ie. it spreads out the array into a list
-//2. When used in a function definition, ... is used as rest op, ie. it packs all list items into an array
-//also - rest params should be last ones in arguments
-//DESTRUCTURING ARRAYS
-var myHobbies = ["cooking", "cycling"];
-var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
-console.log("hobby1: " + hobby1 + " and hobby2: " + hobby2);
+    console.log(makeArray(1, 2, 3, 4, 6));
+    //IMP DIFFERENCE ->
+    //1. When used in a function call, ... is used as a spread operator, ie. it spreads out the array into a list
+    //2. When used in a function definition, ... is used as rest op, ie. it packs all list items into an array
+    //also - rest params should be last ones in arguments
+    //DESTRUCTURING ARRAYS
+    var myHobbies = ["cooking", "cycling"];
+    var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
+    console.log("hobby1: " + hobby1 + " and hobby2: " + hobby2);
+    //DESTRUCTURING OBJECTS
+    var myuserData = { userName: 'John', age: 23 };
+    var myName = myuserData.userName, myAge = myuserData.age;
+    console.log(myName, myAge);
+})(section4 || (section4 = {}));
--- ts-course/section-4.ts
+++ ts-course/section-4.ts
@@ -1,44 +1,48 @@
 /**
  * Created by manishgiri on 10/30/16.
  */
+namespace section4 {
+
+
 //ARROW FUNCTIONS
-console.log("ARROW FUNCTIONS");
-const addNumbers = function(num1: number, num2: number): number {
-    return num1 + num2;
-};
+    console.log("ARROW FUNCTIONS");
+    const addNumbers = function (num1: number, num2: number): number {
+        return num1 + num2;
+    };
 
-console.log(addNumbers(10,15));
+    console.log(addNumbers(10, 15));
 
-const multipliedNumbers = (num1: number, num2: number) => num1 * num2;
-console.log(multipliedNumbers(6,7));
+    const multipliedNumbers = (num1: number, num2: number) => num1 * num2;
+    console.log(multipliedNumbers(6, 7));
 
-const greeter = () => console.log("Hello World!!");
-greeter();
+    const greeter = () => console.log("Hello World!!");
+    greeter();
 
-const greetFriend = friend => console.log(`Hi ${friend}`);
-greetFriend("James");
+    const greetFriend = friend => console.log(`Hi ${friend}`);
+    greetFriend("James");
 
 //default parameters
-const countDown = (start: number = 10): void => {
-    while (start > 0) {
-        console.log(start);
-        start--;
-    }
-    console.log(`Done! ${start}`);
-};
+    const countDown = (start: number = 10): void => {
+        while (start > 0) {
+            console.log(start);
+            start--;
+        }
+        console.log(`Done! ${start}`);
+    };
 //countDown(10);
 //countDown();
 
 //SPREAD OPERATOR
-const numbers = [1,20,3, 55, 99];
-console.log(Math.max(20, 3, 99, 55, 1));
-console.log(Math.max(...numbers));
+    const numbers = [1, 20, 3, 55, 99];
+    console.log(Math.max(20, 3, 99, 55, 1));
+    console.log(Math.max(...numbers));
 
 //REST OPERATOR
-function makeArray(...args: number[]) {
-    return args;
-}
-console.log(makeArray(1,2,3,4,6));
+    function makeArray(...args: number[]) {
+        return args;
+    }
+
+    console.log(makeArray(1, 2, 3, 4, 6));
 
 //IMP DIFFERENCE ->
 //1. When used in a function call, ... is used as a spread operator, ie. it spreads out the array into a list
@@ -46,6 +50,12 @@ console.log(makeArray(1,2,3,4,6));
 //also - rest params should be last ones in arguments
 
 //DESTRUCTURING ARRAYS
-const myHobbies = ["cooking", "cycling"];
-const [hobby1, hobby2] = myHobbies;
-console.log(`hobby1: ${hobby1} and hobby2: ${hobby2}`);
+    const myHobbies = ["cooking", "cycling"];
+    const [hobby1, hobby2] = myHobbies;
+    console.log(`hobby1: ${hobby1} and hobby2: ${hobby2}`);
+
+//DESTRUCTURING OBJECTS
+    let myuserData = {userName: 'John', age: 23};
+    let {userName: myName, age: myAge} = myuserData;
+    console.log(myName, myAge);
+}
