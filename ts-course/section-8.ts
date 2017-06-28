e3aee1d6923e57b70602f1d451dbda116bc0555c
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-27 21:24:32 -0400

    Create Generic Function

--- ts-course/section-8.ts
+++ ts-course/section-8.ts
@@ -2,5 +2,19 @@
  * Created by manishgiri on 11/28/16.
  */
 namespace section8 {
+    // simple generic
+    function echo(data: any) {
+        return data;
+    }
 
+    console.log(echo("Max"));
+    console.log(echo(27));
+
+    // better generic
+    function betterEcho<T>(data: T) {
+        return data;
+    }
+
+    console.log(betterEcho("John"));
+    console.log(betterEcho(27));
 }
