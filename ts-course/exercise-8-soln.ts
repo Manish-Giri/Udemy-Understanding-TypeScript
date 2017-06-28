c2a20cf30284c9f81df6b78482217120cff6fe87
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-27 23:52:29 -0400

    Solve Exercise 8

---
+++ ts-course/exercise-8-soln.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by manishgiri on 6/27/17.
+ */
+
+namespace exercise8 {
+
+    // create Map class
+    class MyMap<T> {
+        private map: {[key: string]: T} = {};
+
+        setItem(key: string, item: T) {
+            this.map[key] = item;
+        }
+
+        getItem(key: string): T {
+            return this.map[key];
+        }
+
+        clear() {
+            this.map = {};
+        }
+
+        printMap() {
+            for(let key in this.map) {
+                console.log(`${key}: ${this.map[key]}`);
+            }
+        }
+    }
+
+    const numberMap = new MyMap<number>();
+    numberMap.setItem("zombies", 5);
+    numberMap.setItem("doges", 10);
+
+    console.log(numberMap.getItem("doges"));
+
+}
