e7ebc99d59993104f7f986852af242d8dff564d8
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-27 21:49:34 -0400

    Constraints with Generics, T extends

--- ts-course/section-8.js
+++ ts-course/section-8.js
@@ -1,51 +1,43 @@
 /**
  * Created by manishgiri on 11/28/16.
  */
-namespace section8 {
+var section8;
+(function (section8) {
     // simple generic
-    function echo(data: any) {
+    function echo(data) {
         return data;
     }
-
     console.log(echo("Max"));
     console.log(echo(27));
-
     // better generic
-    function betterEcho<T>(data: T) {
+    function betterEcho(data) {
         return data;
     }
-
     console.log(betterEcho("John"));
     console.log(betterEcho(27));
-
     console.log("****************************");
     // ARRAYS - GENERICS
-
-    let testResults: Array<number> = [1, 2, 4];
+    var testResults = [1, 2, 4];
     testResults.push(10);
     // error
     // testResults.push("one");
     console.log(testResults);
-
     console.log("****************************");
     // GENERIC TYPES
-    const echo2: <T>(data: T) => T = betterEcho;
+    var echo2 = betterEcho;
     echo2("Hi");
-
     console.log("****************************");
     // CONSTRAINTS WITH GENERICS
-
-    class SimpleMath<T extends Number> {
-        baseValue: T;
-        multiplyValue: T;
-        calculate(): number {
-            return +this.baseValue * +this.multiplyValue;
+    var SimpleMath = (function () {
+        function SimpleMath() {
         }
-    }
-
-    let simpleMath = new SimpleMath<number>();
+        SimpleMath.prototype.calculate = function () {
+            return +this.baseValue * +this.multiplyValue;
+        };
+        return SimpleMath;
+    }());
+    var simpleMath = new SimpleMath();
     simpleMath.baseValue = 10;
     simpleMath.multiplyValue = 20;
     console.log(simpleMath.calculate());
-
-}
+})(section8 || (section8 = {}));
